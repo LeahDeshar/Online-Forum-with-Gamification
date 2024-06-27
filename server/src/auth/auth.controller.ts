8c89ff1c5f075e341047ca590eaa01099a394c14
Leah Deshar <leah.desar@gmail.com>
2024-06-27 13:24:07 +0545

    auth controller

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  @Get('google/login')
+  handleLogin() {}
+
+  //   api/auth/google/redirect
+  @Get('google/redirect')
+  handleRedirect() {
+    return { msg: 'OK' };
+  }
+}
