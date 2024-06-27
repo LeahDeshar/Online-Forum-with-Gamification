91567aa87b8ee00b6a928017caa0788a3e29d6d4
Leah Deshar <leah.desar@gmail.com>
2024-06-27 13:37:17 +0545

    auth controller

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
+import { GoogleStrategy } from './utils/GoogleStrategy';
 
 @Module({
-  controllers: [AuthController]
+  controllers: [AuthController],
+  providers: [GoogleStrategy],
 })
 export class AuthModule {}
