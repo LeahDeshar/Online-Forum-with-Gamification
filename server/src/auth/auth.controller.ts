060cba5dff45177f78e3397794e50c7336892b4e
Leah Deshar <leah.desar@gmail.com>
2024-07-02 21:11:53 +0545

    auth controller

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,13 +1,27 @@
-import { Controller, Get } from '@nestjs/common';
-
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { GoogleAuthGuard } from './utils/Guards';
+import { Request } from 'express';
 @Controller('auth')
 export class AuthController {
   @Get('google/login')
-  handleLogin() {}
-
+  @UseGuards(GoogleAuthGuard)
+  handleLogin() {
+    return { msg: 'Google Authentication' };
+  }
   //   api/auth/google/redirect
   @Get('google/redirect')
   handleRedirect() {
     return { msg: 'OK' };
+  }
+
+  // api/auth/google/redirect
+  @Get('status')
+  user(@Req() request: Request) {
+    console.log(request.user);
+    if (request.user) {
+      return { msg: 'Authenticated' };
+    } else {
+      return { msg: 'Not Authenticated' };
+    }
   }
 }
